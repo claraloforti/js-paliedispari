b2c529edac1a6afebb31c51e8338de2a73846a2c
Clara Loforti <clara.loforti@gmail.com>
2025-12-05 15:02:09 +0100

    funzione reversedWord e correzione bug nel file

--- es-palindroma/palindroma.js
+++ es-palindroma/palindroma.js
@@ -2,12 +2,27 @@
 
 // Chiedere all’utente di inserire una parola
 let userWord = prompt("Inserisci una parola!").toLowerCase();
+let wordIsPalindrome;
 
-document.getElementById("parola-user").innerHTML = "La parola che hai scelto è: " + userWord;
+if (reversedWord(userWord) === userWord) {
+    wordIsPalindrome = "La parola che hai scelto è palindroma!";
+} else {
+    wordIsPalindrome = "La parola che hai scelto NON è palindroma!";
+}
 
+document.getElementById("parola-user").innerHTML = "Hai scelto la parola: " + userWord;
+document.getElementById("risultato").innerHTML = wordIsPalindrome;
 
 
 // FUNZIONI
 
 // Funzione per capire se la parola inserita è palindroma
 
+function reversedWord(parola) {
+    let risultato = "";
+    for (let i = parola.length - 1; i >=0; i--) {
+        risultato += parola[i];
+    }
+
+    return risultato;
+}
