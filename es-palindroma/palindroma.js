25422f912466f069c9cdc6da885733c8bb4426f7
Clara Loforti <clara.loforti@gmail.com>
2025-12-05 14:35:29 +0100

    es.palindroma chiedo all'user di inserire una parola

--- es-palindroma/palindroma.js
+++ es-palindroma/palindroma.js
@@ -1,3 +1,13 @@
 // PALINDROMA
+
 // Chiedere all’utente di inserire una parola
-// Creare una funzione per capire se la parola inserita è palindroma
+let userWord = prompt("Inserisci una parola!").toLowerCase();
+
+document.getElementById("parola-user").innerHTML = "La parola che hai scelto è: " + userWord;
+
+
+
+// FUNZIONI
+
+// Funzione per capire se la parola inserita è palindroma
+
