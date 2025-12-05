0dd5a4ab97db392df8fa67cc6c508849fad31c14
Clara Loforti <clara.loforti@gmail.com>
2025-12-05 13:13:38 +0100

    inizializzo repository

---
+++ es-pari-dispari/pari-dispari.js
@@ -0,0 +1,6 @@
+// PARI E DISPARI
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Genero un numero random (da 1 a 5) per il Computer usando una funzione
+// Sommo i due numeri
+// Stabilisco se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiaro chi ha vinto
